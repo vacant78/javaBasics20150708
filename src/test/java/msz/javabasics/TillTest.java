231c6c25636c7765d8e02934abc17217d3f332dd
Maciej Szatanski <maciej.szatanski@betfair.com>
2015-07-09 21:47:10 +0100

    3. Single apple, single orange test cases. Red.

--- src/test/java/msz/javabasics/TillTest.java
+++ src/test/java/msz/javabasics/TillTest.java
@@ -11,11 +11,29 @@ import static org.junit.Assert.assertThat;
 public class TillTest {
 
 
+    public static final int PRICE_APPLE = 60;
+    private static final int PRICE_ORANGE = 25;
+
     @Test
     public void shouldChargeZeroOnEmptyCart(){
         Till till = new Till();
         till.addCart(new Cart());
         int total = till.getTotalInCentisimal();
         assertThat(total, equalTo(0));
+    }
+
+    @Test
+    public void shouldChargeForOneApple(){
+        Till till = new Till();
+        till.addCart(new Cart());
+        int total = till.getTotalInCentisimal();
+        assertThat(total, equalTo(PRICE_APPLE));
+    }
+    @Test
+    public void shouldChargeForOneOrange(){
+        Till till = new Till();
+        till.addCart(new Cart());
+        int total = till.getTotalInCentisimal();
+        assertThat(total, equalTo(PRICE_ORANGE));
     }
 }
