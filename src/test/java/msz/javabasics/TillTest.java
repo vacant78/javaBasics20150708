433b4a3f22da769d753953e4763533b96931e67c
Maciej Szatanski <maciej.szatanski@betfair.com>
2015-07-09 21:44:07 +0100

    2. Simplest test cage. Green

--- src/test/java/msz/javabasics/TillTest.java
+++ src/test/java/msz/javabasics/TillTest.java
@@ -2,6 +2,9 @@ package msz.javabasics;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by SzatanskiM on 09/07/2015.
  */
