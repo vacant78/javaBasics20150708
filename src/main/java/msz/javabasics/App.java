e7315ea905fd2bc1b7a8f22ca3855dcc8364d1fd
Maciej Szatanski <maciej.szatanski@betfair.com>
2015-07-09 22:48:38 +0100

    8. Application Integration Test - throws on invalid input. Green.

---
+++ src/main/java/msz/javabasics/App.java
@@ -0,0 +1,14 @@
+package msz.javabasics;
+
+/**
+ * Created by SzatanskiM on 09/07/2015.
+ */
+public class App {
+
+    public static void main(String[] args){
+        if(args == null || args.length > 1) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
